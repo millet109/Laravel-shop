d956b6b551afa4f4b5c61de1eb16706dd7487cdc
guwenjie0375 <guguguwenjie@163.com>
2019-11-07 17:40:01 +0800

    增加基础布局 增加MIX

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,6 +5,8 @@
  * Date: 2019/11/7
  * Time: 15:54
  */
+use Illuminate\Support\Facades\Route;
+
 /**
  * 测试辅助函数
  * @return string
@@ -12,4 +14,13 @@
 function test_helper()
 {
     return 'OK';
+}
+
+/**
+ * 将当前请求的路由名称转换为 CSS 类名称，允许我们针对某个页面做页面样式定制
+ * @return mixed
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
