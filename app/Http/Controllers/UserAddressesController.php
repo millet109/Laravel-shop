86cf98a822c9fe5a7e63e765a429b66af502259e
guwenjie0375 <guguguwenjie@163.com>
2019-11-11 14:54:16 +0800

    新增收货地址的修改和删除

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -50,4 +50,32 @@ class UserAddressesController extends Controller
         ]));
         return redirect()->route('user_addresses.index');
     }
+
+    public function edit(UserAddress $user_address)
+    {
+        return view('user_addresses.create_and_edit',[
+            'address' => $user_address
+        ]);
+    }
+
+    public function update(UserAddress $user_address,UserAddressRequest $request)
+    {
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    public function destroy(UserAddress $user_address)
+    {
+        $user_address->delete();
+        return redirect()->route('user_addresses.index');
+    }
 }
--- resources/views/user_addresses/index.blade.php
+++ resources/views/user_addresses/index.blade.php
@@ -28,8 +28,12 @@
                                 <td>{{ $address->zip }}</td>
                                 <td>{{ $address->contact_phone }}</td>
                                 <td>
-                                    <button class="btn btn-primary">修改</button>
-                                    <button class="btn btn-danger">删除</button>
+                                    <a href="{{ route('user_addresses.edit',['user_address' => $address->id]) }}" class="btn btn-primary">修改</a>
+                                    <form action="{{ route('user_addresses.destroy', ['user_address' => $address->id]) }}" method="post" style="display: inline-block">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button class="btn btn-danger" type="submit">删除</button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
