a30a4bb094f9700758724750d3bc625714759a75
guwenjie0375 <guguguwenjie@163.com>
2019-11-12 13:55:38 +0800

    新增管理后台：编辑商品

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,7 @@ class Product extends Model
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
 
-    public function sku()
+    public function skus()
     {
         return $this->hasMany(ProductSku::class);
     }
