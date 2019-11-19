186319deacbd2dd5a4573fe6c2be3a3d057176d7
guwenjie0375 <guguguwenjie@163.com>
2019-11-19 11:22:08 +0800

    新增管理后台：订单发货功能

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -27,4 +27,6 @@ Route::group([
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
 
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+
+    $router->post('orders/{order}/ship','OrdersController@ship')->name('admin.orders.ship');
 });
