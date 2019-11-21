56b0f902159df089f79500944139926ea6989f44
guwenjie0375 <guguguwenjie@163.com>
2019-11-21 15:01:52 +0800

    新增用户界面：优惠券可用情况检查

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,8 @@ Route::group(['middleware' => ['auth','verified']],function (){
 
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
 
 });
 /**
